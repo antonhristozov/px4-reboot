912b13118926789ccd6f4dc14d089a7fd7a027de
Anton Hristozov <anton.hristozov@gmail.com>
2021-03-28 20:13:12 -0400

    Needed to change the header fie

--- src/examples/checkpoint/checkpoint_keep.h
+++ src/examples/checkpoint/checkpoint_keep.h
@@ -4,8 +4,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern struct timespec *ckp_checkpoint_timestamp;
-extern struct timespec *ckp_rollback_timestamp;
+extern struct timespec64 *ckp_checkpoint_timestamp;
+extern struct timespec64 *ckp_rollback_timestamp;
 extern int ckpfid;
 extern int ckpid;
 #ifdef __cplusplus
